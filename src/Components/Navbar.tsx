f3547d452ada7c1b20cef850177fa83e2f9ad2d9
IoanaPedroo <ioanaserban.iulia@gmail.com>
2022-07-22 00:08:06 +0300

    implemented navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return(
+        <nav className="navbar-container">
+            <a href="/">
+                Home
+            </a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <a href="/category">Categories</a>
+            <a href="/sign-up">Sign up</a>
+            <a href="/sign-in">Sign in</a>
+        </nav>
+    )
+}
+
+export default Navbar

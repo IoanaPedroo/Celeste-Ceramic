9b5d954a42e7c135e051a8e032f58a745da35afe
IoanaPedroo <ioanaserban.iulia@gmail.com>
2022-07-22 09:57:19 +0300

    aleca implemented product card

--- src/Components/Page-Categories.tsx
+++ src/Components/Page-Categories.tsx
@@ -1,59 +1,40 @@
 import {Avatar, List, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
 import React from "react";
+import ProductCard from "./ProductCard";
+import './ProductCard.css'
 import './Categories.css'
 
 
+export interface Props {
+
+    productName? : string
+    price? : number
+    currency? : string
+    description? : string
+    pictureUrl : string
+
+}
+
 const Categories = () => {
-    let products: ({ price: number; pictureUrl: string; description: string; currency: string; productName: string })[];
-    products = [
-        {
-            productName: 'Cercei stravezii',
-            price: 50,
-            currency: 'lei',
-            description: "handmade pottery",
-            pictureUrl: "/products/five.jpg"
-        },
-        {
-            productName: 'Vaza Ming',
-            price: 50,
-            currency: 'lei',
-            description: "handmade pottery",
-            pictureUrl: "/products/four.jpg"
-        },
-        {
-            productName: 'Cana cherry',
-            price: 50,
-            currency: 'lei',
-            description: "handmade pottery",
-            pictureUrl: "/products/three.jpg"
-        },
-        {
-            productName: 'Cana Toucan',
-            price: 50,
-            currency: 'lei',
-            description: "handmade pottery",
-            pictureUrl: "/products/two.jpg"
-        },
-        {
-            productName: 'Vaza Roma',
-            price: 50,
-            currency: 'lei',
-            description: "handmade pottery",
-            pictureUrl: "/products/one.jpg"
-        },
+    const products = [
+        {productName: 'Cercei stravezii', price: 50, currency: 'lei', description: "handmade pottery", pictureUrl : "/products/five.jpg"},
+        {productName: 'Vaza Ming', price: 50, currency: 'lei', description: "handmade pottery", pictureUrl :  "/products/four.jpg"},
+        {productName: 'Cana cherry', price: 50, currency: 'lei',description: "handmade pottery", pictureUrl :  "/products/three.jpg"},
+        {productName: 'Cana Toucan', price: 50, currency: 'lei', description: "handmade pottery", pictureUrl :  "/products/two.jpg"},
+        {productName: 'Vaza Roma', price: 50, currency: 'lei', description: "handmade pottery", pictureUrl :  "/products/one.jpg"},
+        {productName: 'Vaza Roma', price: 50, currency: 'lei', description: "handmade pottery", pictureUrl :  "/products/one.jpg"},
     ];
     return(
         <div>
             <h1>Products Page</h1>
-            <List>
+            <List className="grid">
 
                 {products.map((product, index) => (
-                    <ListItem key={index}>
-                        <ListItemAvatar>
-                            <Avatar src={product.pictureUrl}/>
-                            <ListItemText>{product.productName} - {product.price} {product.currency} - {product.description} <br></br><button>Add to cart</button></ListItemText>
-                        </ListItemAvatar>
-                    </ListItem>
+                    <div className="element">
+                        <ListItem key={index} />
+                        <ProductCard pictureUrl = {product.pictureUrl} productName = {product.productName}
+                                     price = {product.price} currency = {product.currency} description = {product.description}/>
+                    </div>
                 ))}
             </List>
         </div>
---
+++ src/Components/ProductCard.tsx
@@ -0,0 +1,32 @@
+import { Avatar, Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material"
+import { Props } from "./Page-Categories"
+
+export default function ProductCard({pictureUrl, productName, price, currency, description}:Props){
+    return (
+        <>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    height="150"
+                    image={pictureUrl}
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {productName} {price} {currency}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Add to cart</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+
+        </>
+
+    )
+
+}

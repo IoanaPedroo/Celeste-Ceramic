905b239bb8d537234c1852075535183f3e4f3597
IoanaPedroo <ioanaserban.iulia@gmail.com>
2022-08-08 09:40:50 +0300

    sign up

--- src/Components/Sign-up.tsx
+++ src/Components/Sign-up.tsx
@@ -1,8 +1,28 @@
+import "./Sign-up.css"
+
+
+
 
 const SignUp = () => {
     return(
-        <div></div>
+        <div className="login-wrapper">
+            <h1>Please Log In</h1>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text"/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password"/>
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
     )
 }
+
 
 export default SignUp
